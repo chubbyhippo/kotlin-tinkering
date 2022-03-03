6d837d258a2a84e00d8a40dafd20b62000b04b0f
ch <mkmsacc@outlook.com>
2022-03-03 07:59:37 +0700

    Range and For

---
+++ src/main/kotlin/basics/LoopAndRanges.kt
@@ -0,0 +1,17 @@
+package basics
+
+fun main() {
+    val range = 1..10
+    for (i in range) {
+        println("i = $i")
+    }
+
+    val reverseRange = 10 downTo 1
+    for (i in reverseRange) {
+        println("i = $i")
+    }
+
+    for (i in reverseRange step 2) {
+        println("i = $i")
+    }
+}
