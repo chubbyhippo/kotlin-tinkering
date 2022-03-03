272d8b6df8155b95a54b17490c3c1d93a135cdf0
ch <mkmsacc@outlook.com>
2022-03-03 07:54:41 +0700

    Add conditions

---
+++ src/main/kotlin/basics/Conditions.kt
@@ -0,0 +1,33 @@
+package basics
+
+import kotlin.random.Random
+
+fun main() {
+    val name = readln()
+    val s = if (name.length == 4) {
+        println("name = 4 chars")
+        name
+    } else {
+        println("name != 4 chars")
+        name
+    }
+    println(s)
+
+    val medal = when (Random.nextInt(1, 4)) {
+        1 -> {
+            "GOLD"
+        }
+        2 -> {
+            "SILVER"
+        }
+        3 -> {
+            "BRONZE"
+        }
+        else -> {
+            "NO MEDAL"
+        }
+    }
+
+    println(medal)
+
+}
