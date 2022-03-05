f6fd0f6c4e0ae7a3170b16cb1583593e3867d47a
ch <mkmsacc@outlook.com>
2022-03-05 13:03:35 +0700

    Add more functions

--- src/main/kotlin/functions/Functions.kt
+++ src/main/kotlin/functions/Functions.kt
@@ -1,13 +1,26 @@
 package functions
 
+import java.time.LocalDate
+
 fun main() {
     printName("Hippo")
     val result: Int = addition(1, 2)
     println("result = $result")
+    printPersonDetails(name = "Hippo")
 }
 
 fun addition(x: Int, y: Int) = x + y
 
 fun printName(s: String) {
     println(s)
+}
+
+fun printPersonDetails(
+    name: String,
+    email: String = "",
+    dob: LocalDate = LocalDate.now()
+) {
+    println("name = $name")
+    println("email = $email")
+    println("dob = $dob")
 }
