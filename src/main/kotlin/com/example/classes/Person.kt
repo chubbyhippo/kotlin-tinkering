dfa6e2ddae22fefee6b6fe1c8a09b66984ff8f58
ch <mkmsacc@outlook.com>
2022-03-06 17:21:11 +0700

    Add Constructor

--- src/main/kotlin/com/example/classes/Person.kt
+++ src/main/kotlin/com/example/classes/Person.kt
@@ -1,12 +1,21 @@
 package com.example.classes
 
-class Person {
+class Person(
+    val name: String = "",
+    val age: Int = 20
+) {
     fun action() {
         println("Hippo walks")
     }
 }
 
 fun main() {
-    val person = Person()
+    val person = Person("Hippo", 99)
     person.action()
+    println(
+        """
+        Name: ${person.name}
+        Age: ${person.age}
+    """.trimIndent()
+    )
 }
