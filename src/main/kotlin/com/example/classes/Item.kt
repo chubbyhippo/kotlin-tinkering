fed590a6f773b6f99621f01f69dd0c37542b1083
ch <mkmsacc@outlook.com>
2022-03-07 09:11:20 +0700

    Add getter/setter

--- src/main/kotlin/com/example/classes/Item.kt
+++ src/main/kotlin/com/example/classes/Item.kt
@@ -2,6 +2,19 @@ package com.example.classes
 
 class Item {
     var name: String = ""
+    var price: Double = 0.0
+        get() {
+            println("Inside Getter")
+            return field
+        }
+        set(value) {
+            println("Inside Setter")
+            if (value >= 0.0) {
+                field = value
+            } else {
+                throw IllegalArgumentException("Negative price is not allowed")
+            }
+        }
 
     constructor(name: String) {
         this.name = name
@@ -12,4 +25,7 @@ fun main() {
     val item = Item("Ipad")
     println("item = $item")
     println(item)
+    item.price = -1.0
+    item.price = 2.0
+    println(item.price)
 }
