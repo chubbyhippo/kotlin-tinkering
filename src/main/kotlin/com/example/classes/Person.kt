2fa19cd45dfb2ce18495e15a8fd5404eae1a33ac
ch <mkmsacc@outlook.com>
2022-03-07 07:41:03 +0700

    Add Secondary constructor

--- src/main/kotlin/com/example/classes/Person.kt
+++ src/main/kotlin/com/example/classes/Person.kt
@@ -4,6 +4,12 @@ class Person(
     val name: String = "",
     val age: Int = 20
 ) {
+    var email: String = ""
+
+    constructor(email: String) : this() {
+        this.email = email
+    }
+
     fun action() {
         println("Hippo walks")
     }
@@ -18,4 +24,11 @@ fun main() {
         Age: ${person.age}
     """.trimIndent()
     )
+
+    val person2 = Person("hippo@gmail.com")
+    println("""
+        Name: ${person2.name}
+        Age: ${person2.age}
+        email: ${person2.email} 
+    """.trimIndent())
 }
