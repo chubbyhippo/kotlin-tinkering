8f0b889dcb8832238d70817b33df6e73486669ab
ch <mkmsacc@outlook.com>
2022-03-14 08:33:51 +0700

    Add run

--- src/main/kotlin/com/example/nulls/ExploreNulls.kt
+++ src/main/kotlin/com/example/nulls/ExploreNulls.kt
@@ -2,10 +2,14 @@ package com.example.nulls
 
 fun main() {
     var nameNullable: String? = null
-    println("nameNullable = ${nameNullable?.length}")
+//    println("nameNullable = ${nameNullable?.length}")
+//    printName(nameNullable!!)
+//    val length = nameNullable?.length?.toLong() ?: 0
+//    println("length = $length")
 
-    val length = nameNullable?.length?.toLong() ?: 0
-    println("length = $length")
+    nameNullable?.run {
+        println("length = $length")
+    }
 
     nameNullable = "Hippo"
     println("nameNullable = $nameNullable")
@@ -25,3 +29,7 @@ data class Movie(
     val id: Int?,
     val name: String
 )
+
+fun printName(name: String) {
+    println("name = [${name}]")
+}
