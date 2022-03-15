feeb97bb8bd037384f922fb6d4f3f667302ffd35
ch <mkmsacc@outlook.com>
2022-03-15 08:17:04 +0700

    Add High order function

--- src/main/kotlin/com/example/collections/ExploreLambdas.kt
+++ src/main/kotlin/com/example/collections/ExploreLambdas.kt
@@ -8,8 +8,20 @@ fun main() {
 
     val multiplyLambda = { x: Int, y: Int ->
         println("x = [${x}], y = [${y}]")
-        x * y }
+        x * y
+    }
 
     val multiplyResult = multiplyLambda(3, 4)
     println("multiplyResult = $multiplyResult")
+
+    val calculate = calculate(2, 3) { x: Int, y: Int -> x * y }
+    println("calculate = $calculate")
+
+
+    val add = calculate(2, 3) { x: Int, y: Int -> x + y }
+    println("add = $add")
+}
+
+fun calculate(x: Int, y: Int, op: (x: Int, y: Int) -> Int): Int {
+    return op(x, y)
 }
