ce4f749f1d463c497822b85065dee8cf13acb71e
ch <mkmsacc@outlook.com>
2022-03-15 07:29:03 +0700

    Add mutable list

--- src/main/kotlin/com/example/collections/ExploreCollections.kt
+++ src/main/kotlin/com/example/collections/ExploreCollections.kt
@@ -2,6 +2,9 @@ package com.example.collections
 
 fun main() {
     val names = listOf("Hippo", "Giraffe", "Kangaroo")
-    println("names = ${names}")
+    println("names = $names")
+
+    val namesMutableList = mutableListOf("Hippo", "Giraffe", "Kangaroo")
+    println("namesMutableList = $namesMutableList")
 
 }
