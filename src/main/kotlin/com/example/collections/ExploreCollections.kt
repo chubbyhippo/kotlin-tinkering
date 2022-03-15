eee00e38966b0248662052cad6243201405cfa72
ch <mkmsacc@outlook.com>
2022-03-15 07:56:59 +0700

    Add mutable set

--- src/main/kotlin/com/example/collections/ExploreCollections.kt
+++ src/main/kotlin/com/example/collections/ExploreCollections.kt
@@ -12,4 +12,9 @@ fun main() {
 
     val set = setOf("Hippo", "Giraffe", "Kangaroo")
     println("set = $set")
+
+    val mutableSet = mutableSetOf("Hippo", "Giraffe", "Kangaroo")
+    println("mutableSet = $mutableSet")
+    mutableSet.add("Wombat")
+    println("mutableSet = $mutableSet")
 }
