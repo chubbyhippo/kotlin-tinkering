21c194e5f528d84869ef356d4b308c7a8578734f
ch <mkmsacc@outlook.com>
2022-03-20 11:31:01 +0700

    Add exploreFilterUsingSequence

--- src/main/kotlin/com/example/collections/CollectionOperationsUsingSequences.kt
+++ src/main/kotlin/com/example/collections/CollectionOperationsUsingSequences.kt
@@ -1,5 +1,9 @@
 package com.example.collections
 
+import com.example.dataset.Course
+import com.example.dataset.CourseCategory
+import com.example.dataset.courseList
+
 fun main() {
     val namesListUsingSequence = listOf("Hippo", "Kangaroo", "Giraffe", "Lion")
         .asSequence()
@@ -9,4 +13,17 @@ fun main() {
 
     println("namesListUsingSequence = $namesListUsingSequence")
 
+    val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPMENT }
+
+    exploreFilterUsingSequence(courseList(), devPredicate)
+
+}
+
+fun exploreFilterUsingSequence(courseList: MutableList<Course>, devPredicate: (Course) -> Boolean) {
+    courseList
+        .asSequence()
+        .filter(devPredicate)
+        .forEach {
+            println("Courses = $it")
+        }
 }
