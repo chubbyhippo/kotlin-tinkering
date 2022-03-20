801c1383685d641bba3c495256f30d0cf881052a
ch <mkmsacc@outlook.com>
2022-03-20 14:05:04 +0700

    Explore let

--- src/main/kotlin/com/example/scopefunctions/ExploreScopeFunctions.kt
+++ src/main/kotlin/com/example/scopefunctions/ExploreScopeFunctions.kt
@@ -7,7 +7,29 @@ fun main() {
 
     exploreApply()
     exploreAlso()
+    exploreLet()
 
+}
+
+fun exploreLet() {
+    val numbers = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val result = numbers
+        .map { it * 2 }
+        .filter { it > 5 }
+        .let {
+            println("it = $it")
+            it.sum()
+        }
+    println("result = $result")
+
+    var name:String? = null
+    name = "Hippo"
+
+    val result1 = name?.let {
+        println("it = $it")
+        it.uppercase()
+    }
+    println("result1 = $result1")
 }
 
 fun exploreApply() {
