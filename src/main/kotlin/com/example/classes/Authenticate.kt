775f758cca42ebf8d5e4517a244e7d3328282d0b
ch <mkmsacc@outlook.com>
2022-05-27 07:54:55 +0700

    Add JvmStatic

--- src/main/kotlin/com/example/classes/Authenticate.kt
+++ src/main/kotlin/com/example/classes/Authenticate.kt
@@ -1,6 +1,7 @@
 package com.example.classes
 
 object Authenticate {
+    @JvmStatic
     fun authenticate(userName: String, password: String) {
         println("userName = $userName")
         println("password = $password")
