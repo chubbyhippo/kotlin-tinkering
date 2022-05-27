e91b89085309dc54cc2f53194641ad6d2dcf94f6
ch <mkmsacc@outlook.com>
2022-05-27 07:42:51 +0700

    Add jvmoverload

--- src/main/java/com/example/InvokeKotlinFromJava.java
+++ src/main/java/com/example/InvokeKotlinFromJava.java
@@ -2,6 +2,7 @@ package com.example;
 
 import com.example.classes.Course;
 import com.example.classes.CourseCategory;
+import com.example.classes.CourseKt;
 
 public class InvokeKotlinFromJava {
     public static void main(String[] args) {
@@ -12,5 +13,7 @@ public class InvokeKotlinFromJava {
                 CourseCategory.DEVELOPMENT
         );
         System.out.println("course : " + course);
+
+        CourseKt.printName();
     }
 }
